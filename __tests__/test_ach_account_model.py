0e5d56d96f598a8c8db6eb90f92317c7f01d8b40
Justin Chiou <chiou.kai@gmail.com>
2021-12-21 12:57:18 -0500

    fix: add more tests for ACH accounts

--- __tests__/test_ach_account_model.py
+++ __tests__/test_ach_account_model.py
@@ -3,7 +3,35 @@ from app.models import AchAccount
 
 def test_instance():
     ach_account = AchAccount(
-        account="123",
-        routing="123456789",
+        account="1234",
+        routing="021000021",
     )
     assert isinstance(ach_account, AchAccount)
+
+
+def test_ach_account_valid_account_number_edge_cases():
+    ach_account_three_digits = AchAccount(
+        account="123",
+        routing="021000021",
+    )
+    assert len(ach_account_three_digits.account) == 3
+
+    ach_account_seventeen_digits = AchAccount(
+        account="12345678901234567",
+        routing="021000021",
+    )
+    assert len(ach_account_seventeen_digits.account) == 17
+
+
+def test_ach_account_different_routing_numbers():
+    routing_numbers = [
+        "021000021",
+        "011401533",
+        "091000019",
+    ]
+    for r in routing_numbers:
+        ach_account = AchAccount(
+            account="123",
+            routing=r,
+        )
+        assert len(ach_account.routing) == 9
