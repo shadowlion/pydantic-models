63dccd898da65ce570f1109d00bd5136ddfe2e4e
Justin Chiou <chiou.kai@gmail.com>
2021-12-21 12:57:05 -0500

    refactor: document app code

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import date
 from pydantic import BaseModel, validator
 
+from app.helpers import validate_aba_routing_number
+
 
 class CreditCard(BaseModel):
     """
-    Pydantic model that currently accepts only the following:
+    Credit Card pydantic model. Ccurrently accepts only the following:
     VISA, DISCOVER, MASTERCARD, and AMEX.
     However, we also invalidate AMEX from the final solution.
     """
@@ -16,14 +18,27 @@ class CreditCard(BaseModel):
     cvv: str
 
     @property
-    def brand(cls):
-        """Checks the credit card number, returns the brand it's from."""
-        # number = cls.number
+    def brand(cls) -> str:
+        """Checks the credit card number, returns the brand it's from.
+
+        Returns:
+            str: credit card company (abbr)
+        """
         pass
 
     @validator("name")
     def validate_name(cls, name: str) -> str:
-        """Checks the name is at least 2 characters; no numbers in the name"""
+        """Validates cardholder name
+
+        Args:
+            name (str): cardholder name
+
+        Raises:
+            ValueError: Name length must be greater than 1 character
+
+        Returns:
+            str: cardholder name
+        """
         if len(name.strip()) < 2:
             raise ValueError("Names must be at least 2 letters.")
         return name
@@ -34,14 +49,34 @@ class CreditCard(BaseModel):
 
     @validator("month")
     def validate_month(cls, month: str) -> str:
-        """Checks that the month index is between 1 and 12."""
+        """Validate expiry month
+
+        Args:
+            month (str): expiry month
+
+        Raises:
+            ValueError: Must be between 1 and 12
+
+        Returns:
+            str: expiry month
+        """
         if not month.isdigit() or int(month) < 1 or int(month) > 12:
             raise ValueError("Must choose a number between 1 and 12.")
         return month
 
     @validator("year")
     def validate_year(cls, year: str) -> str:
-        """Checks that the input year is a year in the future, including this year."""
+        """Validate expiry year
+
+        Args:
+            year (str): expiry year
+
+        Raises:
+            ValueError: Year must be between {current year} and {current year + 10}
+
+        Returns:
+            str: expiry year
+        """
         current_year = date.today().year
         if (
             not year.isdigit() or
@@ -55,33 +90,64 @@ class CreditCard(BaseModel):
 
     @validator("cvv")
     def validate_cvv(cls, cvv: str) -> str:
-        """Checks that the cvv is a 3-digit number."""
+        """Validate cvv number
+
+        Args:
+            cvv (str): cvv number
+
+        Raises:
+            ValueError: Must be a valid 3-digit number
+
+        Returns:
+            str: cvv number
+        """
         if not cvv.isdigit() or len(cvv) != 3:
             raise ValueError("Must be a valid 3-digit number.")
         return cvv
 
 
 class AchAccount(BaseModel):
-    """Pydantic model that validates an ACH account."""
+    """ACH Account pydantic model."""
 
     account: str
     routing: str
 
-    @property
-    def bank(cls):
-        """Runs an API call to verify the routing number is tied to a real bank."""
-        pass
-
     @validator("account")
     def validate_account_number(cls, num: str) -> str:
+        """Validate the account number input.
+
+        Args:
+            num (str): account number
+
+        Raises:
+            ValueError: Account numbers must be between 3 and 17 digits
+
+        Returns:
+            str: account number
+        """
         if not num.isdigit() or len(num) < 3 or len(num) > 17:
             raise ValueError("Account numbers must be between 3 and 17 digits.")
         return num
 
     @validator("routing")
     def validate_routing_number(cls, num: str) -> str:
+        """Validate the routing number input.
+
+        Args:
+            num (str): routing number
+
+        Raises:
+            ValueError: Routing numbers must be 9 digits
+            ValueError: Routing number is invalid
+
+        Returns:
+            str: routing number
+        """
         if not num.isdigit() or len(num) != 9:
             raise ValueError("Routing numbers must be 9 digits.")
+        r = validate_aba_routing_number(num)
+        if r.get("statusCode") == "215":
+            raise ValueError("Invalid routing number")
         return num
 
 
@@ -104,12 +170,34 @@ class Accreditation(BaseModel):
 
     @validator("annual_income")
     def validate_annual_income(cls, num: int) -> int:
+        """Validate annual income
+
+        Args:
+            num (int): annual income
+
+        Raises:
+            ValueError: Must be greater than 0
+
+        Returns:
+            int: annual income
+        """
         if num < 0:
             raise ValueError("Must be greater than zero.")
         return num
 
     @validator("net_worth")
     def validate_net_worth(cls, num: int) -> int:
+        """Validate net worth
+
+        Args:
+            num (int): net worth
+
+        Raises:
+            ValueError: Must be greater than 0
+
+        Returns:
+            int: net worth
+        """
         if num < 0:
             raise ValueError("Must be greater than zero.")
         return num
