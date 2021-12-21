9cd91198c0952613af1b60ddb0c059e7eb7b5d24
Justin Chiou <chiou.kai@gmail.com>
2021-12-21 15:11:00 -0500

    refactor: add more tests for ach account model

--- __tests__/test_ach_account_model.py
+++ __tests__/test_ach_account_model.py
@@ -1,37 +1,98 @@
+import pytest
+
+from pydantic import ValidationError
+
 from app.models import AchAccount
 
 
-def test_instance():
-    ach_account = AchAccount(
-        account="1234",
+def test_model_instance():
+    ach = AchAccount(
+        account="123456789",
         routing="021000021",
     )
-    assert isinstance(ach_account, AchAccount)
+    assert isinstance(ach, AchAccount)
 
 
-def test_ach_account_valid_account_number_edge_cases():
-    ach_account_three_digits = AchAccount(
+def test_model_no_inputs():
+    with pytest.raises(ValidationError):
+        AchAccount()
+
+
+def test_model_account_number_input_only():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            account="123",
+        )
+
+
+def test_model_routing_number_input_only():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            routing="123456789",
+        )
+
+
+def test_model_account_number_two_digits():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            account="12",
+            routing="021000021"
+        )
+
+
+def test_model_account_number_three_digits():
+    ach = AchAccount(
         account="123",
         routing="021000021",
     )
-    assert len(ach_account_three_digits.account) == 3
+    assert ach.account == "123"
+    assert ach.routing == "021000021"
 
-    ach_account_seventeen_digits = AchAccount(
-        account="12345678901234567",
+
+def test_model_account_number_seventeen_digits():
+    ach = AchAccount(
+        account="123",
         routing="021000021",
     )
-    assert len(ach_account_seventeen_digits.account) == 17
+    assert ach.account == "123"
+    assert ach.routing == "021000021"
 
 
-def test_ach_account_different_routing_numbers():
-    routing_numbers = [
+def test_model_account_number_eighteen_digits():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            account="123456789012345678",
+            routing="021000021",
+        )
+
+
+def test_model_routing_number_eight_digits():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            account="123",
+            routing="02100002",
+        )
+
+
+def test_model_routing_number_nine_digits():
+    routing_numbers = (
         "021000021",
         "011401533",
         "091000019",
-    ]
-    for r in routing_numbers:
-        ach_account = AchAccount(
+    )
+    for i, r in enumerate(routing_numbers):
+        ach = AchAccount(
             account="123",
             routing=r,
         )
-        assert len(ach_account.routing) == 9
+        assert len(ach.routing) == 9
+        assert ach.account == "123"
+        assert ach.routing == routing_numbers[i]
+
+
+def test_model_routing_number_ten_digits():
+    with pytest.raises(ValidationError):
+        AchAccount(
+            account="123",
+            routing="0210000210",
+        )
